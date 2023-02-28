2bd4edb0fa347f478e8d85757fd4b51c724c0ffd
Amit Yadav <faizabadamit26@gmail.com>
2023-02-28 12:53:50 +0530

    add country

--- backend/routes/emp.js
+++ backend/routes/emp.js
@@ -5,18 +5,21 @@ const { body, validationResult } = require('express-validator');
 
 router.post('/createemp', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
-  body('age', 'Enter a valid age').isLength({ min: 1 }),
-  body('email', 'Enter a valid email').isLength({ min: 13 }),
+  body('age', 'Enter a valid age').isInt({ min: 10 }),
+  body('email', 'Enter a valid email').isEmail({ min: 10 }),
   body('salary', 'Enter a valid salary').isLength({ min: 3 }),
   body('country', 'Enter a valid country').isLength({ min: 1 }),
   body('state', 'Enter a valid state').isLength({ min: 1 }),
   body('city', 'Enter a valid city').isLength({ min: 1 }),
 ], async (req, res) => {
   //if there are errors, return Bad request and the errors
-  console.log(req.body, "req");
+  // console.log(req.body, "req");
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    let msg = errors.array();
+    let msg1 = msg[0].msg;
+    console.log(msg1,"eroooo");
+    return res.status(400).json({ errors: msg1, error: "401" });
   }
   try {
     // Check whether the user with this employee exists already
--- src/app/components/Index.js
+++ src/app/components/Index.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import Employ from './Employ';
+import { Country, State, City } from 'country-state-city'
 // import Update from './Update';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Index = () => {
 
+    const [country, setCountry] = useState([])
+    const [state, setState] = useState([])
+    const [city, setCity] = useState([])
     const [credentials, setCredentials] = useState({ name: "", age: "", email: "", salary: "", country: "", state: "", city: "" })
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(credentials);
+        console.log(credentials,"credentials");
         const { name, age, email, salary, country, state, city } = credentials;
         const response = await fetch(`http://localhost:5000/api/emp/createemp`, {
             method: 'POST',
@@ -20,8 +24,19 @@ const Index = () => {
             body: JSON.stringify({ name, age, email, salary, country, state, city })
         });
         const json = await response.json()
-        if(json.error=="404")
-        {
+        let msg = json.errors;
+        if (json.error === "401") {
+            toast.error(msg, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+        else if (json.error == "404") {
             toast.error('Employee Already Exist!', {
                 position: "top-right",
                 autoClose: 5000,
@@ -32,7 +47,7 @@ const Index = () => {
                 progress: undefined,
             });
         }
-        else if(json.success) {
+        else if (json.success) {
             toast.success('Your record inserted!', {
                 position: "top-right",
                 autoClose: 5000,
@@ -59,6 +74,48 @@ const Index = () => {
         // setCredentials({name:"e.target.name" , district:"e.target.email", place:"e.target.district", nearground:"e.target.nearground", distance:"e.target.distance" ,password:"e.target.password",confirmpassword:"e.target.confirmpassword"})
         setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
+    useEffect(() => {
+    const countries = Country.getAllCountries()
+    let p = [];
+    for(let i=0;i<countries.length;i++)
+    {
+           p.push(countries[i].name);
+        //    console.log(countries[i].name);
+    }
+    setCountry(p);
+    const states = State.getAllStates()
+    let p1 = [];
+    for(let i=0;i<states.length;i++)
+    {
+           p1.push(states[i].name);
+        //    console.log(states[i].name);
+    }
+    setState(p1);
+    // const cities = City.getAllCities();
+    // let p2 = [];
+    // for(let i=0;i<cities.length;i++)
+    // {
+    //        p1.push(cities[i].name);
+    //     //    console.log(cities[i].name);
+    // }
+    // setCity(p2);
+    // console.log(State.getStateByCodeAndCountry("UP", "IN"))
+    // const cities = City.getAllCities()
+    // const newCity = City.getCitiesOfState("IN","UP")
+    // console.log(newCity[0].name)
+    // for(let i=0;i<cities.length;i++)
+    // {
+    //         console.log(newCity[i].name);
+    // }
+    // for(let i=0;i<cities.length;i++)
+    // {
+    //     if(cities[i].countryCode=="IN")
+    //     {
+    //       console.log(cities[i]);
+    //     }
+    // }
+    // console.log(State.getCityByCode("IN"));
+    }, []);
     return (
         <div className="container" style={{ marginTop: "100px" }}>
             <ToastContainer />
@@ -76,21 +133,33 @@ const Index = () => {
                                         onChange={onChange} type="text" id="name" name="name" className="border-b-2 border-stone-400 text-stone-400 w-36" />
                                     <p className="text-sm text-stone-400 mt-6">Age</p> <input
                                         onChange={onChange} type="number" id="age" name="age" className="border-b-2 border-stone-400 text-stone-400 w-36" />
-                                    <p className="text-sm text-stone-400 mt-6">Country</p> <input
-                                        onChange={onChange} type="text" id="country" name="country" className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                                    {/* <p className="text-sm text-stone-400 mt-6">Country</p> <input
+                                        onChange={onChange} type="text" id="country" name="country" className="border-b-2 border-stone-400 text-stone-400 w-36" /> */}
+                                    <select className="form-select my-3" name="place" defaultValue={'DEFAULT'} onChange={onChange} aria-label="Default select example">
+                                        <option value="Default">Choose Your Country</option>
+                                        {country.map(place => <option key={place} value={place}>{place}</option>)}
+                                    </select>
                                 </div>
                                 <div className="m-6 ">
                                     <p className="text-sm text-stone-400">E-mail</p> <input
-                                        onChange={onChange} type="text" id="email" name="email" className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                                        onChange={onChange} type="email" id="email" name="email" className="border-b-2 border-stone-400 text-stone-400 w-36" />
                                     <p className="text-sm text-stone-400 mt-6">Salary</p> <input
                                         onChange={onChange} type="number" id="salary" name="salary" className="border-b-2 border-stone-400 text-stone-400 w-36" />
-                                    <p className="text-sm text-stone-400 mt-6">State</p> <input
-                                        onChange={onChange} type="text" id="state" name="state" className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                                    {/* <p className="text-sm text-stone-400 mt-6">State</p> <input
+                                        onChange={onChange} type="text" id="state" name="state" className="border-b-2 border-stone-400 text-stone-400 w-36" /> */}
+                                        <select className="form-select my-3" defaultValue={'DEFAULT'} name="place" onChange={onChange} aria-label="Default select example">
+                                        <option value="Default">Choose Your State</option>
+                                        {state.map(place => <option key={place} value={place}>{place}</option>)}
+                                    </select>
                                 </div>
                             </div>
                             <div className="m-6 ">
                                 <p className="text-sm text-stone-400 mt-6 ">City</p> <input
                                     onChange={onChange} type="text" id="city" name="city" className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                                    {/* <select className="form-select my-3" name="place" onChange={onChange} aria-label="Default select example">
+                                        <option selected>Choose Your State</option>
+                                        {city.map(place => <option key={place} value={place}>{place}</option>)}
+                                    </select> */}
                                 <button type="submit" onClick={handleSubmit} className="group relative my-3 flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                                     <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                                     </span>
